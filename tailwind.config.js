aac3278c585101c83c2242f49308c5796d9ef139
Jabran Rafique <jabran.rafique@ratedpeople.com>
2024-02-12 23:37:53 +0000

    update homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     extend: {
       backgroundImage: ({ theme }) => ({
         skyBackground:
-          'linear-gradient(90deg, rgba(252,176,69,1) 0%, rgba(253,29,29,1) 50%, rgba(131,58,180,1) 100%)'
+          'linear-gradient(90deg,#ff7800,#f80032 17.19%,#ff00a0 42.71%,#8c28ff 60%,#0023ff 85%,#1798ff)'
       })
     }
   },
