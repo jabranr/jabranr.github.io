34252f3ded4419eb0aff7b9ad62836316c85dc2e
Jabran Rafique <2131246+jabranr@users.noreply.github.com>
2026-01-30 16:42:30 +0000

    chore: update test config, ignore readme

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './tests',
+  testDir: './integration',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -12,20 +12,20 @@ module.exports = defineConfig({
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:8080',
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
 
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+      use: { ...devices['Desktop Chrome'] }
+    }
   ],
 
   webServer: {
-    command: 'npm run build && npx @11ty/eleventy --serve --port=8080',
+    command: 'npm run build && npm run preview',
     url: 'http://localhost:8080',
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+    timeout: 120 * 1000
+  }
 });
