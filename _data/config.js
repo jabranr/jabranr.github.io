03353b6ff5d2dc05002abaccd1ed765ac9647462
Jabran Rafique <2131246+jabranr@users.noreply.github.com>
2026-01-31 09:19:58 +0000

    chore: use text file for version tracking

--- _data/config.js
+++ _data/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   APP_HOSTNAME: process.env.APP_HOSTNAME,
-  GTM_ID: process.env.GTM_ID,
-  VERSION: process.env.GIT_VERSION || 'dev'
+  GTM_ID: process.env.GTM_ID
 };
