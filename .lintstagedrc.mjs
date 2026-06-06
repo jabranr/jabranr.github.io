6b021d0d5c4fd00574a07f5a6c6e3b7e41cae9db
Jabran Rafique <jabranr@users.noreply.github.com>
2026-06-06 14:49:01 +0100

    chore(resume): update for next roles (#88)
    Co-authored-by: Jabran Rafique <2131246+jabranr@users.noreply.github.com>

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 import { lintStagedCommands } from '@jabraf/dev/lint-staged';
 
 export default {
-  '*.{md,js,njk,yaml,yml,json,xml,css}': (files) => [lintStagedCommands.prettier(files)],
+  '*.{md,js,yaml,yml,json,xml,css}': (files) => [lintStagedCommands.prettier(files)],
 };
